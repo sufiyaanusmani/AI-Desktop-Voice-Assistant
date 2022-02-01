56485681dbe163b330c79850f90c631fd3749a87
Sufiyaan Usmani <usmanisufiyaan@gmail.com>
2022-02-01 19:23:17 +0500

    added voice recognition feature and browser options

--- assistant.py
+++ assistant.py
@@ -1,5 +1,9 @@
 import pyttsx3
+import speech_recognition as sr
 import datetime
+import wikipedia
+import webbrowser
+
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
@@ -22,5 +26,39 @@ def welcome():
 
     speak("How may I help you")
 
+def takeCommand():
+    """It takes microphone input from the user and returns string output"""
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 0.8
+        audio = r.listen(source)
+    
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en-us')
+        print(f"User said: {query}")
+    except Exception as e:
+        speak("Say that again please")
+        print("Say that again please")
+        return "None"
+    return query
+
 if __name__ == "__main__":
     welcome()
+    while True:
+        query = takeCommand().lower()
+
+        if "wikipedia" in query:
+            speak("Searching Wikipedia...")
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to Wikipedia, ")
+            print(results)
+            speak(results)
+        elif "open youtube" in query or "youtube" in query:
+            webbrowser.open("youtube.com")
+        elif "open google" in query or "google" in query:
+            webbrowser.open("google.com")
+        elif "stackoverflow" in query:
+            webbrowser.open("stackoverflow.com")
