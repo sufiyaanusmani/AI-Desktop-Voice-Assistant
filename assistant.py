931d9cfcbb0d7f48bcbc32d94416f88eb656b1d6
Sufiyaan Usmani <usmanisufiyaan@gmail.com>
2022-02-01 19:56:25 +0500

    added more options

--- assistant.py
+++ assistant.py
@@ -3,11 +3,13 @@ import speech_recognition as sr
 import datetime
 import wikipedia
 import webbrowser
+import os
+# import smtplib
 
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)
+engine.setProperty('voice', voices[0].id)
 rate = engine.getProperty('rate')
 engine.setProperty('rate', 175)
 
@@ -44,6 +46,12 @@ def takeCommand():
         return "None"
     return query
 
+# def sendEmail(to, content):
+#     server = smtplib.SMTP("smtp.gmail.com", 587)
+#     server.ehlo()
+#     server.starttls()
+
+
 if __name__ == "__main__":
     welcome()
     while True:
@@ -57,8 +65,33 @@ if __name__ == "__main__":
             print(results)
             speak(results)
         elif "open youtube" in query or "youtube" in query:
+            speak("Opening youtube")
             webbrowser.open("youtube.com")
         elif "open google" in query or "google" in query:
+            speak("Opening google")
             webbrowser.open("google.com")
         elif "stackoverflow" in query:
             webbrowser.open("stackoverflow.com")
+        elif "play music" in query:
+            speak("Opening music")
+            webbrowser.open_new("https://www.youtube.com/watch?v=60ItHLz5WEA&ab_channel=AlanWalker") # just a sample
+        elif "time" in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f"Sir, the time is {strTime}")
+        elif "open vs code" in query:
+            codePath = "C:\\Users\\Sufyan\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+            speak("Opening VS Code...")
+            os.startfile(codePath)
+        # elif "send email" in query:
+        #     try:
+        #         speak("What shoud I say?")
+        #         content = takeCommand()
+        #         to = "suffiyaanusmani@gmail.com"
+        #         sendEmail(to, content)
+        #         speak("Email has been sent")
+        #     except Exception as e:
+        #         print(e)
+        #         speak("Sorry, email not sent")
+        elif "quit" in query:
+            speak("Bye")
+            break
